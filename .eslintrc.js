63b39a7fd1e9ab0d8f0f9983118ff41b17bb3a51
DavideBruner <davidebruner@hotmail.it>
2021-04-09 11:34:19 +0200

    chore(eslinte): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,46 @@
 const path = require('path');
 
 module.exports = {
-  extends: [
-    'airbnb-base',
-    'airbnb'
-  ],
+  extends: ["airbnb", "prettier"],
+  plugins: ["prettier", "react-hooks"],
   parser: 'babel-eslint',
-  env: {
-    browser: true,
-    node: true
+  rules: {
+    // Prettier are errors
+    "prettier/prettier": ["error"],
+    // Allow JSX on JS Files (React only)
+    "react/jsx-filename-extension": [
+      1,
+      {
+        "extensions": [".js", ".jsx"]
+      }
+    ],
+    "no-plusplus": [
+      "error",
+      {
+        "allowForLoopAfterthoughts": true
+      }
+    ],
+    "no-continue": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        "js": "never",
+        "jsx": "never",
+        "ts": "never",
+        "tsx": "never"
+      }
+    ],
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 0,
+    'import/order': 0,
+    'import/prefer-default-export': 0,
+    'no-console': [2, { allow: ['error', 'info', 'log'] }],
   },
-  plugins: [
-    'chai-friendly',
-    'react-hooks',
-  ],
+  env: {
+    "browser": true,
+    "node": true
+  },
   settings: {
     'import/resolver': {
       'eslint-import-resolver-lerna': {
@@ -24,41 +51,5 @@ module.exports = {
       }
     }
   },
-  rules: {
-    'arrow-parens': [2, 'as-needed'],
-    'chai-friendly/no-unused-expressions': 2,
-    'consistent-return': 0,
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      },
-    ],
-    'import/no-extraneous-dependencies': 0,
-    'import/no-named-as-default': 0,
-    'import/order': 0,
-    'import/prefer-default-export': 0,
-    'no-console': [2, { allow: ['error', 'info'] }],
-    "no-multiple-empty-lines": [2, { "max": 1 }],
-    'no-param-reassign': [2, { props: false }],
-    'no-plusplus': 0,
-    'no-shadow': 0,
-    'no-unused-expressions': 0,
-    'object-curly-newline': [2, { multiline: true, consistent: true }],
-    'react/jsx-filename-extension': [2, {
-      extensions: ['.js', '.ts', '.tsx']
-    }],
-    'no-nested-ternary': 0,
-    'react/jsx-sort-default-props': 2,
-    'react/sort-prop-types': 2,
-    'react-hooks/rules-of-hooks': 2,
-    'jsx-a11y/label-has-for': 0, // temporary until airbnb remove this deprecated rule from their configuration
-    'jsx-a11y/label-has-associated-control': [2, {
-      assert: 'either', // Because we use custom react components the nesting option cannot be respected for all cases
-    }],
-  }
-}
+};
+
--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   plugins: [
-    '@babel/proposal-class-properties',
-    '@babel/proposal-export-default-from',
-    '@babel/plugin-proposal-optional-chaining',
+    "@babel/proposal-class-properties",
+    "@babel/proposal-export-default-from",
+    "@babel/plugin-proposal-optional-chaining",
   ],
   presets: [
     [
-      '@babel/env',
+      "@babel/env",
       {
         modules: false,
         targets: {
-          node: 'current',
+          node: "current",
         },
       },
     ],
-    '@babel/react',
-  ]
+    "@babel/react",
+  ],
 };
